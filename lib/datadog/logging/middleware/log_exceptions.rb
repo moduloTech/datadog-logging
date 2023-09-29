bf4b952cfd871bce5e005aa5eb276e6a712ca548
Matthieu Ciappara <ciappa_m@modulotech.fr>
2023-09-29 16:03:23 +0200

    Fix undefined method 'log_rescued_responses?' for Datadog::Logging::Middleware::LogExceptions

--- lib/datadog/logging/middleware/log_exceptions.rb
+++ lib/datadog/logging/middleware/log_exceptions.rb
@@ -14,7 +14,7 @@ module Datadog
           logger = logger(request)
 
           return unless logger
-          return if !log_rescued_responses?(request) && wrapper.rescue_response?
+          return if !log_rescued_responses?(request) && rescue_response?(wrapper)
 
           exception = wrapper.exception
 
@@ -23,6 +23,23 @@ module Datadog
                            kind: exception.class.name, message: exception.message, stack: exception.backtrace
                          }
                        })
+        end
+
+        def rescue_response?(wrapper)
+          if wrapper.respond_to?(:rescue_response?)
+            # From Rails 7.0
+            wrapper.rescue_response?
+          else
+            # Before Rails 7.0
+            wrapper.rescue_responses.key?(wrapper.exception.class.name)
+          end
+        end
+
+        # Starting with Rails 7.0, method is defined.
+        unless method_defined?(:log_rescued_responses?)
+          def log_rescued_responses?(request)
+            request.get_header('action_dispatch.log_rescued_responses')
+          end
         end
 
       end
